c684a28c14d4b51aa73390f5f1a0fdeeaf0285eb
FAYE1502 <fayemodou1502@gmail.com>
2026-07-02 04:31:15 +0000

    function genered reference

--- index.php
+++ index.php
@@ -76,3 +76,15 @@ function saveProduct(){
 
 
 }
+
+function genererReference( array $products):string{
+    $taille=count($products)+1;
+     if($taille<=9){
+        $ref="REF00";
+    }elseif ($taille<=99){
+        $ref="REF0";
+    }else{
+        $ref="REF";
+    }
+    return $ref.$taille;
+}
